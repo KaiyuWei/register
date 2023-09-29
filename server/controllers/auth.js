fe867eca5f57ad8ec567206f714645adbb33234c
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-29 19:33:59 +0200

    password check

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,72 @@
 /**
  * the controllers for authentication process
  */
+import mysql from "mysql";
+import validator from "email-validator";
+import * as authHelper from "./helpers/authHelper.js";
+
+/**
+ * user pre-register
+ */
+export const preRegister = async (req, res) => {
+  try {
+    // the email and password from the request body
+    const { email, password } = req.body;
+
+    // validate the email and the password format
+    if (!validator.validate(email)) {
+      return res.json({ error: "A valid email is required" });
+    }
+
+    // validate the password format. if validation fails, we stop here.
+    if (!(authHelper.passwordFormat(password) === true)) return;
+
+    // check if the email has been registerd
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.json({ error: "Email is taken" });
+    }
+
+    // the token for user identification
+    const token = jwt.sign({ email, password }, config.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    // the email body content
+    const content = `
+    <p>Click the link below to activate your account</p>
+    <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate account</a>`;
+
+    // send the email
+    config.AWSSES.sendEmail(
+      // the helper function returns the first argument of the ::sendEmail() method
+      emailTemplate(email, content, config.REPLY_TO, "Activate your account"),
+
+      // error handler
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.json({ ok: false });
+        } else {
+          console.log(data);
+          return res.json({ ok: true });
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "something went wrong" });
+  }
+};
+
+/**
+ * user register
+ */
+
+/**
+ * user password reset
+ */
+
+/**
+ * user login
+ */
---
+++ server/controllers/helpers/authHelper.js
@@ -0,0 +1,52 @@
+/**
+ * helper functions in the authentication process
+ */
+
+/**
+ * validate the password format. This should be done in the frontend. We add it here
+ * just in case.
+ */
+export const passwordFormat = (password) => {
+  // the password is required
+  if (!password) {
+    return res.json({
+      errorCode: 1,
+      error: "password is required",
+    });
+  }
+
+  // the password should be longer than 6 characters
+  if (password.length < 6) {
+    return res.json({
+      errorCode: 2,
+      error: "password should be at least 6 characters",
+    });
+  }
+
+  // the password should contain lower-case characters
+  if (!/[a-z]/.test(password)) {
+    return res.json({
+      errorCode: 3,
+      error: "password should contain at least one lower-case character",
+    });
+  }
+
+  // the password should contain upper-case characters
+  if (!/[A-Z]/.test(password)) {
+    return res.json({
+      errorCode: 3,
+      error: "password should contain at least one upper-case character",
+    });
+  }
+
+  // the password should contain numeric characters
+  if (!/[0-9]/.test(password)) {
+    return res.json({
+      errorCode: 3,
+      error: "password should contain at least one number",
+    });
+  }
+
+  // all check passed
+  return true;
+};
