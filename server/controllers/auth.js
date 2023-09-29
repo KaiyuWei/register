27e7eed2e1e9cc2d028fc818b84f07fcdf01d8f4
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-29 19:42:13 +0200

    password check tested

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -19,40 +19,40 @@ export const preRegister = async (req, res) => {
     }
 
     // validate the password format. if validation fails, we stop here.
-    if (!(authHelper.passwordFormat(password) === true)) return;
+    if (!(authHelper.passwordFormat(password, res) === true)) return;
 
-    // check if the email has been registerd
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.json({ error: "Email is taken" });
-    }
+    //     // check if the email has been registerd
+    //     const user = await User.findOne({ email });
+    //     if (user) {
+    //       return res.json({ error: "Email is taken" });
+    //     }
 
-    // the token for user identification
-    const token = jwt.sign({ email, password }, config.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    //     // the token for user identification
+    //     const token = jwt.sign({ email, password }, config.JWT_SECRET, {
+    //       expiresIn: "1h",
+    //     });
 
-    // the email body content
-    const content = `
-    <p>Click the link below to activate your account</p>
-    <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate account</a>`;
+    //     // the email body content
+    //     const content = `
+    //     <p>Click the link below to activate your account</p>
+    //     <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate account</a>`;
 
-    // send the email
-    config.AWSSES.sendEmail(
-      // the helper function returns the first argument of the ::sendEmail() method
-      emailTemplate(email, content, config.REPLY_TO, "Activate your account"),
+    //     // send the email
+    //     config.AWSSES.sendEmail(
+    //       // the helper function returns the first argument of the ::sendEmail() method
+    //       emailTemplate(email, content, config.REPLY_TO, "Activate your account"),
 
-      // error handler
-      (err, data) => {
-        if (err) {
-          console.log(err);
-          return res.json({ ok: false });
-        } else {
-          console.log(data);
-          return res.json({ ok: true });
-        }
-      }
-    );
+    //       // error handler
+    //       (err, data) => {
+    //         if (err) {
+    //           console.log(err);
+    //           return res.json({ ok: false });
+    //         } else {
+    //           console.log(data);
+    //           return res.json({ ok: true });
+    //         }
+    //       }
+    //     );
   } catch (err) {
     console.log(err);
     return res.json({ error: "something went wrong" });
