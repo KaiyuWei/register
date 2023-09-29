a896e125d096cf7cf93ab5d9f5edb0f148cacbea
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-29 22:31:04 +0200

    email template function and pre-register function added

--- server/controllers/helpers/authHelper.js
+++ server/controllers/helpers/authHelper.js
@@ -50,3 +50,42 @@ export const passwordFormat = (password, res) => {
   // all check passed
   return true;
 };
+
+/**
+ * a function that returns options for AWSSES sendEmail method
+ */
+export const emailTemplate = (email, content, replyTo, subject) => {
+  // the style used for the email template
+  const style = `
+    background: #eee;
+    padding: 30px;
+    border-radius 20px;
+`;
+  return {
+    Source: config.EMAIL_FROM,
+    Destination: {
+      ToAddresses: [email],
+    },
+    ReplyToAddresses: [replyTo],
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `
+                        <html>
+                            <div style="${style}">
+                            <h1>Welcome to Realist</h1>
+                            ${content}
+                            <p>&copy; ${new Date().getFullYear()}</p>
+                            </div>
+                        </html>
+                    `,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: subject,
+      },
+    },
+  };
+};
