326d5b3d09e81d951f744a6941f48ac6a88b89a2
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-29 20:49:50 +0200

    added db module

---
+++ server/DB/db.js
@@ -0,0 +1,20 @@
+/**
+ * this file handles the database connection
+ */
+import mysql from "mysql";
+import "dotenv/config";
+
+// database connection configuration
+const config = {
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.PORT,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
+};
+
+// the db connection instance
+const pool = mysql.createPool(config);
+
+// export the pool
+export default pool;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import mysql from "mysql";
 import AuthRouter from "./routes/auth.js";
 
 // create the app
@@ -19,26 +18,6 @@ app.use(morgan("dev"));
 app.use(cors());
 // use the router middleware
 app.use("/api", AuthRouter);
-
-// database connection configuration
-const config = {
-  host: process.env.RDS_HOSTNAME,
-  port: process.env.PORT,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-};
-
-// the db connection instance
-const connection = mysql.createConnection(config);
-
-// connect to MYSQL server on AWS
-connection.connect(function (err) {
-  if (err) {
-    console.error("Database connection failed: " + err.stack);
-    return;
-  }
-  console.log("DATABASE CONNECTED");
-});
 
 // listen to http requests
 app.listen(8000, () => console.log("SERVER READY ON PORT 8000"));
