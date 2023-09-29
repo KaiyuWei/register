62afa61bd794f6d9689c666b592122a07b15827f
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-29 18:40:49 +0200

    configured the database connection

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@
  * This is the entry of the server when it starts.
  */
 import "dotenv/config";
-console.log(process.env);
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mysql from "mysql";
 
 // create the app
 const app = express();
@@ -23,7 +23,31 @@ app.get("/api", (req, res) => {
   });
 });
 
-const mysql = require("mysql");
+// database connection configuration
+const config = {
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.PORT,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+};
+
+// the db instance
+// const db = mysql.getInstance(config);
+
+const connection = mysql.createConnection(config);
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+
+  console.log("Connected to database.");
+});
+
+// db.connect()
+//   .then(() => console.log("DB CONNECTED"))
+//   .catch((err) => console.log(err));
 
 // listen to http requests
 app.listen(8000, () => console.log("Hello world"));
