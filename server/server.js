13f477d0226bdfe4b6505be4bfe5d24dabb5199a
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-29 17:49:26 +0200

    app set up and packages installed

---
+++ server/server.js
@@ -0,0 +1,25 @@
+/**
+ * This is the entry of the server when it starts.
+ */
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// create the app
+const app = express();
+
+// middleware for parsing the json payload
+app.use(express.json());
+// log HTTP requests info in "dev" mode
+app.use(morgan("dev"));
+// avoid cors error
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({
+    data: "hello world",
+  });
+});
+
+// listen to http requests
+app.listen(8000, () => console.log("Hello world"));
