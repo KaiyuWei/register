56d95c10f5276e4c1799ab63e1da35c92f6ea276
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-30 14:23:31 +0200

    middleware for checking duplicate emails added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,13 +3,16 @@
  */
 import express from "express";
 import * as auth from "../controllers/auth.js";
+import { duplicateEmail } from "../middlewares/auth.js";
 
 // the router instance from express
 const router = express.Router();
 
 // pre-register will send the user an email including an activation link
-router.post("/pre-register", auth.preRegister);
+router.post("/pre-register", duplicateEmail, auth.preRegister);
 // register the user and store the user data in the database
 router.post("/register", auth.register);
+// user login
+router.post("/login", auth.login);
 
 export default router;
