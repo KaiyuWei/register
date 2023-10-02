7a27863f2eb3c8c40904e6d71411ce5aff66e7f0
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-02 08:52:58 +0200

    user registration optimized

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -10,7 +10,6 @@ import axios from "axios";
 export default function PrivateRoute() {
   // context
   const { auth, setAuth } = useAuth();
-  // if we have the user logged in, the state is set to 'true'.
 
   // the outlet enables the nested UI rendering, which requires authentication.
   return auth ? <Outlet /> : "";
