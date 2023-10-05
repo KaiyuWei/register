617f3fff01395ab748e4739a6591f96c8ae8c288
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-05 16:05:17 +0200

    hide some pages when user is logged in

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/auth.js";
 
 export default function Register() {
   // the hooks for states
@@ -12,6 +13,8 @@ export default function Register() {
   const [lastName, setLastName] = useState("");
   // the state indicating the loading status
   const [loading, setLoading] = useState(false);
+  // context
+  const [auth, setAuth] = useAuth();
   // navigation hook
   const navigate = useNavigate();
 
@@ -42,10 +45,6 @@ export default function Register() {
         toast.success("Please check your email to activate your account!");
         // navigate to login
         navigate("/login");
-
-        /**
-         * @todo update login time here
-         */
       }
     } catch (err) {
       // loading process terminated
@@ -55,65 +54,71 @@ export default function Register() {
     }
   };
 
-  return (
-    <div>
-      <div className="container d-flex align-items-center justify-content-center">
-        <h1 className="display-1 text-secondary p-5">Register</h1>
-      </div>
+  // for logged in users, navigate to dashboard page
+  if (auth) {
+    navigate("/dashboard");
+    return;
+  } else {
+    return (
+      <div>
+        <div className="container d-flex align-items-center justify-content-center">
+          <h1 className="display-1 text-secondary p-5">Register</h1>
+        </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 offset-lg-4">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="First name"
-                className="form-control mb-4"
-                required
-                autoFocus
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Last name"
-                className="form-control mb-4"
-                required
-                autoFocus
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Email"
-                className="form-control mb-4"
-                required
-                autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="form-control mb-4"
-                required
-                autoFocus
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button
-                disabled={loading}
-                className="btn btn-primary col-12 mb-4"
-              >
-                {loading ? "Waiting..." : "Register"}
-              </button>
-            </form>
-            <Link className="text-link" to="/login">
-              Have an account? Log in
-            </Link>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 offset-lg-4">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="First name"
+                  className="form-control mb-4"
+                  required
+                  autoFocus
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Last name"
+                  className="form-control mb-4"
+                  required
+                  autoFocus
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="form-control mb-4"
+                  required
+                  autoFocus
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="form-control mb-4"
+                  required
+                  autoFocus
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button
+                  disabled={loading}
+                  className="btn btn-primary col-12 mb-4"
+                >
+                  {loading ? "Waiting..." : "Register"}
+                </button>
+              </form>
+              <Link className="text-link" to="/login">
+                Have an account? Log in
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
